6d977081c458a362bb1fd457909d3b0255e76062
Ashutosh Kesharwani <ashutoshkesharwani20021406@gmail.com>
2026-01-20 18:45:19 +0530

    updated script.js

--- day-04-flex-Panel/script.js
+++ day-04-flex-Panel/script.js
@@ -1,20 +1,22 @@
 const panels = document.querySelectorAll(".panel");
 
       function toggleOpen() {
-        // Close all other panels first
+        // Close all other panels first i.e which are open already close them
         panels.forEach((panel) => {
           if (panel !== this) {
             panel.classList.remove("open", "open-active");
           }
         });
 
-        // Toggle current panel
+        // Toggle current panel [now open current one]
         this.classList.toggle("open");
 
-        // Handle transition end only once
+        // Handle transition end only once 
         this.addEventListener("transitionend", function handler(e) {
           if (e.propertyName.includes("flex")) {
             this.classList.toggle("open-active");
+
+            // remove this event after executed so that it will not listen always
             this.removeEventListener("transitionend", handler);
           }
         });
